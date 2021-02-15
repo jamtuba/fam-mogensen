b6b2d1d0856793cf52e7a8d2700ee3010d942f28
jamtuba <jamtuba@gmail.com>
2021-02-15 15:24:18 +0100

    Added Blazored Modal Added some Image tests

--- fam-mogensen/Program.cs
+++ fam-mogensen/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Blazored.Modal;
 
 namespace fam_mogensen
 {
@@ -26,6 +27,7 @@ namespace fam_mogensen
                 builder.Configuration.Bind("Local", options.ProviderOptions);
             });
 
+            builder.Services.AddBlazoredModal();
 
             await builder.Build().RunAsync();
         }
