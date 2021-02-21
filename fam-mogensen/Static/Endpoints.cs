bccdcd06712bfd90d56231a3cb3e62aa4a1a1723
jamtuba <jamtuba@gmail.com>
2021-02-21 15:18:40 +0100

    Added Static routing to images path

---
+++ fam-mogensen/Static/Endpoints.cs
@@ -0,0 +1,13 @@
+﻿namespace fam_mogensen.Static
+{
+    public static class Endpoints
+    {
+        //public static string BaseUrl = "https://localhost:7071/";
+
+        public static string ImagesEndpoint { get; set; } = "/api/GetImages/";
+        //public static string AuthorsEndpoint { get; set; } = $"{BaseUrl}api/authors/";
+        //public static string BooksEndpoint { get; set; } = $"{BaseUrl}api/books/";
+        //public static string RegisterEndpoint { get; set; } = $"{BaseUrl}api/users/register/";
+        //public static string LoginEndpoint { get; set; } = $"{BaseUrl}api/users/login/";
+    }
+}
