cd8d0f17040023058ef109e3355ac095ba8bac5a
jamtuba <jamtuba@gmail.com>
2021-06-13 11:39:46 +0200

    Small changes to LR file

--- fam-mogensen/Program.cs
+++ fam-mogensen/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace fam_mogensen
 {
-    public class Program
+    public static class Program
     {
         public static async Task Main(string[] args)
         {
